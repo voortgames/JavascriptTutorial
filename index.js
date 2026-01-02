0429b6d01a57827cce1f796a99a692194a4f1a20
Natalie Langevoort <neufae@yahoo.com>
2026-01-02 15:04:45 -0500

    07

--- index.js
+++ index.js
@@ -6,10 +6,20 @@
 // let count = firstBatch + secondBatch
 // console.log(count)
 
-let myAge = 34
-let humanDogRatio = 7
+//let myAge = 34
+//let humanDogRatio = 7
 
-let myDogAge = myAge * humanDogRatio
+//let myDogAge = myAge * humanDogRatio
 
-console.log(myDogAge)
+//console.log(myDogAge)
+
+let count = 5
+
+// count + 1
+count = count + 2
+
+// count - 1
+
+
+console.log(count)
 
