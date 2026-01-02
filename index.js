79a1cab28c288355d8fb334ef1eba71bcf1451b9
Natalie Langevoort <neufae@yahoo.com>
2026-01-02 11:51:00 -0500

    Javascript Tutorial

---
+++ index.js
@@ -0,0 +1,15 @@
+// document.getElementById("count").innerText = 5
+
+// ctrl+k+c
+// let firstBatch = 5
+// let secondBatch = 7
+// let count = firstBatch + secondBatch
+// console.log(count)
+
+let myAge = 34
+let humanDogRatio = 7
+
+let myDogAge = myAge * humanDogRatio
+
+console.log(myDogAge)
+
